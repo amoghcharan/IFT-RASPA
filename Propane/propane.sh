367a1d42ea922ae45a0096c53a38452474829fb5
amoghcharan <62420154+amoghcharan@users.noreply.github.com>
2022-05-17 09:03:13 -0500

    propane

---
+++ Propane/propane.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#  propane.sh
+#  
+#
+#  Created by Amogh Chinnakonda on 5/8/22.
+#  
+cd /Users/amogh/Downloads/'ME 497'/RASPA2/examples/Bulk-Density/Propane
+#cd '1'
+#echo "First Pressure Started"
+#./run
+#echo "First Pressure Finished"
+#
+#cd ..
+#cd '2'
+#echo "Second Pressure Started"
+#./run
+#echo "Second Pressure Finished"
+#
+#cd ..
+#cd '3'
+#echo "Third Pressure Started"
+#./run
+#echo "Third Pressure Finished"
+
+#cd ..
+cd '4'
+echo "Fourth Pressure Started"
+./run
+echo "Fourth Pressure Finished"
+
+cd ..
+cd '5'
+echo "Fifth Pressure Started"
+./run
+echo "Fifth Pressure Finished"
+
+#cd ..
+#cd '6'
+#echo "Sixth Pressure Started"
+#./run
+#echo "Sixth Pressure Finished"
+#
